d5f600f7e162a50f2b285f4ea1a437aee91b69ed
Justincadburywong <justincadburywong@gmail.com>
2016-10-17 19:02:27 -0700

    edit index to send response text

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,22 @@
 get '/' do
   erb :index
 end
+
+post '/reply' do
+  message_body = params["Body"]
+  from_number = params["From"]
+  boot_twilio
+  sms = @client.messages.create(
+    # from: Rails.application.secrets.twilio_number,
+    to: from_number,
+    body: "Hello there, thanks for texting me. Your number is #{from_number}."
+  )
+end
+
+private
+
+def boot_twilio
+  account_sid = TWILIO_SID
+  auth_token = TWILIO_TOKEN
+  @client = Twilio::REST::Client.new account_sid, auth_token
+end
