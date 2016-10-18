567f8213c5af4fd0f6c7ee5336a7c2d761b87781
Justincadburywong <justincadburywong@gmail.com>
2016-10-17 19:28:02 -0700

    fix route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,22 +3,13 @@ get '/' do
 end
 
 post '/reply' do
-  message_body = params["Body"]
-  from_number = params["From"]
-  twilio_number = "14152752620"
-  boot_twilio
-  sms = @client.messages.create(
-    # from: Rails.application.secrets.twilio_number,
-    from: twilio_number,
-    to: from_number,
-    body: "Hello there, thanks for texting me. Your number is #{from_number}."
-  )
+  send_welcome_text
 end
 
 private
 
-def boot_twilio
-  account_sid = TWILIO_SID
-  auth_token = TWILIO_TOKEN
-  @client = Twilio::REST::Client.new account_sid, auth_token
-end
+# def boot_twilio
+#   account_sid = TWILIO_SID
+#   auth_token = TWILIO_TOKEN
+#   @client = Twilio::REST::Client.new account_sid, auth_token
+# end
