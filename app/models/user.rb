d05eb78c7c5e688fb44628f32c590efe3a4950ba
Justincadburywong <justincadburywong@gmail.com>
2016-10-18 00:50:39 -0700

    temp remove validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,22 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   include BCrypt
   has_secure_password
-  before_save { self.full_name = email.downcase }
+  before_save { self.full_name = full_name.downcase }
   before_save { self.address = address.downcase }
   before_save { self.city = city.downcase }
   before_save { self.state = state.downcase }
 
-  VALID_PHONE_NUMBER_REGEX = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
-  validates :phone, uniqueness: true,
-                    presence: true,
-                    length: { maximum: 15 },
-                    format: { with: VALID_PHONE_NUMBER_REGEX}
-
-  VALID_ACCOUNT_NUMBER_REGEX = /\d{16}/
-  validates :account_number, uniqueness: true,
-                    presence: true,
-                    length: { maximum: 16, minimum: 16 },
-                    format: { with: VALID_ACCOUNT_NUMBER_REGEX}
+  # VALID_PHONE_NUMBER_REGEX = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+  # validates :phone, uniqueness: true,
+  #                   presence: true,
+  #                   length: { maximum: 15 },
+  #                   format: { with: VALID_PHONE_NUMBER_REGEX}
+  #
+  # VALID_ACCOUNT_NUMBER_REGEX = /\d{16}/
+  # validates :account_number, uniqueness: true,
+  #                   presence: true,
+  #                   length: { maximum: 16, minimum: 16 },
+  #                   format: { with: VALID_ACCOUNT_NUMBER_REGEX}
 
   validates :password, presence: true,
                     length: { minimum: 6 }
